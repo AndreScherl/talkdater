b044b879120a0f2206c6166654ae9d7d52d72a90
Andre Scherl <post@schoerlock.de>
2019-01-06 02:14:58 +0100

    migrated to newest ooth version

--- api/index.js
+++ api/index.js
@@ -1,26 +1,27 @@
-const {MongoClient, ObjectId} = require('mongodb')
-const express = require('express')
-const session = require('express-session')
-const {promisify} = require('util')
-const Ooth = require('ooth')
-const oothLocal = require('ooth-local')
-const oothLocalTalkdater = require('./ooth-local-talkdater')
-const OothMongo = require('ooth-mongo')
-const config = require('config')
-const mail = require('./mail')
-const emailer = require('./ooth-local-emailer-talkdater')
+const { MongoClient } = require("mongodb")
+const express = require("express")
+const session = require("express-session")
+const { Ooth } = require("ooth")
+const oothLocal = require("ooth-local").default
+const oothUser = require("ooth-user").default
+const emailer = require("ooth-local-emailer").default
+const { OothMongo } = require("ooth-mongo")
+const config = require("config")
+const mail = require("./mail")
 
-const DEFAULT_LANGUAGE = 'de'
+const DEFAULT_LANGUAGE = "de"
 
 const start = async () => {
     try {
         // connect to mongo db
         const mongo_cfg = config.get("mongo")
-        const client = await MongoClient.connect(mongo_cfg.url + '/' + mongo_cfg.db, {
+        const client = await MongoClient.connect(mongo_cfg.url, {
             auth: {
                 user: mongo_cfg.user,
-                password: mongo_cfg.pwd,
+                password: mongo_cfg.pwd
             },
+            authSource: mongo_cfg.db,
+            useNewUrlParser: true
         })
         const db = client.db(mongo_cfg.db)
         
@@ -36,18 +37,28 @@ const start = async () => {
         // prevent x-powered-by attacks (see http://expressjs.com/en/advanced/best-practice-security.html#use-helmet)
         app.disable('x-powered-by')
 
+        // connect ooth to mongodb backend with own api.
+        const oothMongo = new OothMongo(db)
+        
         // new ooth instance - main instance for user authentication
         const ooth = new Ooth({
+            app,
+            backend: oothMongo,
             sharedSecret: config.get("ooth.sharedSecret"),
             path: config.get("ooth.path"),
         })
 
-        // connect ooth to mongodb backend with own api, and start ooth.
-        const oothMongo = new OothMongo(db, ObjectId)
-        await ooth.start(app, oothMongo)
+        oothLocal({
+            ooth
+        })
+
+        oothUser({
+            ooth
+        })
 
         // settings for email interaction, including localization.
-        const mailOnEvents = emailer({
+        emailer({
+            ooth,
             from: config.get("mail.from"),
             siteName: config.get("siteName"),
             url: config.get("host"),
@@ -63,14 +74,35 @@ const start = async () => {
             language: 'de'
         })
 
-        // add the local authentication method to ooth instance. Customized for TalkDater.
-        ooth.use('local', oothLocalTalkdater(oothLocal(mailOnEvents), mailOnEvents.onVerify))
+        // some events that may be implemented soon      
+        ooth.on("local", "register", ({ email, verificationToken, _id }) => {
+            console.log(`Someone registered.`)
+        });
+        ooth.on(
+            "local",
+            "generate-verification-token",
+            ({ email, verificationToken }) => {
+                console.log(`Someone requested a verification email.`);
+            }
+        );
+        ooth.on("local", "verify", ({ email }) => {
+            console.log(`Someone verified their email`);
+        });
+        ooth.on("local", "forgot-password", ({ email, passwordResetToken }) => {
+            console.log(`Someone forgot their password`);
+        });
+        ooth.on("local", "reset-password", ({ email }) => {
+            console.log(`Someone reset their password`);
+        });
 
         // define route for main entry of app.
-        app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
+        app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
 
         // start listenting for requests.
-        await promisify(app.listen)(config.get("port"))
+        await new Promise((res, rej) =>
+            //app.listen(config.get("port"), config.get("host"), e => (e ? rej(e) : res(e)))
+            app.listen(config.get("port"), e => (e ? rej(e) : res(e)))
+            );
         console.info(`Online at ${config.get("host")}:${config.get("port")}`)
     } catch (e) {
         console.error(e)
